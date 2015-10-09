ebb94c71c990b5ef771cab7e85f3f97ebce9af37
Irwing <khalisser@gmail.com>
2015-10-09 13:41:02 -0430

    Estructura maquetado de una página de intro

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- Titulo -->
+		<title>Dashboard</title>
+
+		<!-- Includes styles -->
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+		<!-- Includes metas -->
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		
+		<!-- Main Block -->
+		<div class="main">
+			
+			<!-- Nav -->
+			<nav>
+				<a href="#">Introducción</a>
+			</nav>
+			<!-- Finished Nav -->
+			
+			<!-- Header -->
+			<header>
+				<h1>FlexBox</h1>
+				<h3>Aprendiendo todo sobre la nueva etiqueta</h3>
+			</header>
+			<!-- Finished Header -->
+
+			<!-- Content Block -->
+			<section class="content">
+			</section>
+			<!-- Finish Content Block -->
+			
+			<!-- Footer Block -->
+			<footer>
+				Test used flex-box <a href="#">Intro</a>
+			</footer>
+			<!-- Finish Footer Block -->
+
+		</div>
+		<!-- Finish Main Block -->
+
+	</body>
+</html>

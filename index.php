f6d21fb333af645fa4dad572987331f5558bea88
Irwing <khalisser@gmail.com>
2015-10-09 22:42:59 -0430

    Agregado animaciones

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 		<!-- Includes styles -->
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link rel="stylesheet" type="text/css" href="css/intro.css">
+		<link rel="stylesheet" type="text/css" href="css/animated.css">
 
 		<!-- Includes metas -->
 		<meta charset="UTF-8">
@@ -18,14 +19,14 @@
 
 			<!-- Header -->
 			<header class="margin1">
-				<img src="images/macbook.png" class="logo" />
-				<h1>Mac Store</h1>
-				<h3>innovative technology</h3>
+				<img src="images/macbook.png" class="logo animated fadeInDown" />
+				<h1 class="animated fadeInDown">Mac Store</h1>
+				<h3 class="animated fadeInDown">innovative technology</h3>
 			</header>
 			<!-- Finished Header -->
 
 			<!-- Nav -->
-			<nav class="flex margin2">
+			<nav class="flex margin2 animated fadeInDown">
 				<a href="website.html" class="bg1 margin1 padding1">Website</a>
 				<a href="dashboard.html" class="bg1 margin1 padding1">Store</a>
 			</nav>
